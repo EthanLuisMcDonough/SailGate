b88565e740b959fb6b68a4445584d73ae886e5b1
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2024-11-17 00:48:14 -0600

    chore: Move extractor to Sema package

--- sailgate.sh
+++ sailgate.sh
@@ -8,5 +8,6 @@ ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 interp.csh "$ROOT/src/driver.psi" "$ROOT/src/prelude/prelude.psi" \
     "$ROOT/src/prelude/util.psi" "$ROOT/src/prelude/scalar.psi" \
     "$ROOT/src/prelude/vec.psi" "$ROOT/src/prelude/module.psi" \
-    "$ROOT/src/extractor.psi" "$ROOT/src/prelude/*.psl" \
+    "$ROOT/src/sema/sema.psi" "$ROOT/src/sema/extractor.psi" \
+    "$ROOT/src/prelude/*.psl" "$ROOT/src/sema/*.psl" \
     "$ROOT/src/*.psl" "$@" -command Main

d4d28e91b4f2c76c64a8f38b152d8827cc00bb9c
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2024-11-28 01:38:31 -0600

    fix: Make Async abstract
    Async really shouldn't be a realized type because we don't want the user doing things like Logic<Async>, they should be tethering their type to a clock domain parameter. Unfortunately, some compile-time checks aren't running for types that reference module params. This will do for now.
    
    TODO: Investigate why compile-time checks aren't running for from_univ calls on parameterized types (e.g. Logic<D>, where D is the type param on the enclosing module).

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -4,6 +4,7 @@ import os
 import lit.formats
 
 test_dir = os.path.dirname(__file__)
+shared_dir = os.path.join(test_dir, "Shared")
 base_dir = os.path.abspath(os.path.join(test_dir, os.pardir))
 driver_script = os.path.join(base_dir, "sailgate.sh")
 
@@ -12,6 +13,21 @@ driver_script = os.path.join(base_dir, "sailgate.sh")
 
 config.name = "SailGate"
 config.suffixes = [".psi", ".psl"]
+config.excludes = ["Shared"]
 config.test_source_root = test_dir
 config.test_format = lit.formats.ShTest()
-config.substitutions.append(("%sailgate_check", "echo \"\" | " + driver_script + " %s 2>&1 | FileCheck %s"))
+
+dbg_console_prefix = "echo \"\" | "
+filecheck_suffix = " %s 2>&1 | FileCheck %s"
+fake_dom = os.path.join(shared_dir, "fake_domain.psi")
+
+# %sailgate_typecheck: Runs check for simple type-based tests.
+#                      These tests exist to check how RTL types
+#                      can be used
+config.substitutions.append(("%sailgate_typecheck", dbg_console_prefix +
+                             driver_script + " " + fake_dom +
+                             filecheck_suffix))
+
+# %sailgate_check: Runs driver with test file and checks output
+config.substitutions.append(("%sailgate_check", dbg_console_prefix +
+                             driver_script + filecheck_suffix))
